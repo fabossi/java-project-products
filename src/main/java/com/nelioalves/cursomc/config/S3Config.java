867a0c5086489bf440bf44b84c463aad29ab32e1
Felipe Fabossi <fabossif@gmail.com>
2020-07-15 20:30:49 -0300

    [FABOSSI] Java springboot backend ready

---
+++ src/main/java/com/nelioalves/cursomc/config/S3Config.java
@@ -0,0 +1,25 @@
+package com.nelioalves.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+	
+	@Value("${aws.access_key_id}")
+	private String awsId;
+
+	@Value("${aws.secret_access_key}")
+	private String awsKey;
+
+	@Value("${s3.region}")
+	private String region;
+	
+//	@Bean
+//	public AmazonS3 s3client() {
+//		BasicAWSCredentials awsCred = new BasicAWSCredentials(awsId, awsKey);
+//		AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+//							.withCredentials(new AWSStaticCredentialsProvider(awsCred)).build();
+//		return s3client;
+//	}
+}
---
+++ src/main/java/com/nelioalves/cursomc/services/S3Service.java
@@ -0,0 +1,41 @@
+package com.nelioalves.cursomc.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class S3Service {
+
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+
+//	@Autowired
+//	private AmazonS3 s3client;
+
+//	@Value("${s3.bucket}")
+//	private String bucketName;
+
+//	public URI uploadFile(MultipartFile multipartFile) {
+//		try {
+//			String fileName = multipartFile.getOriginalFilename();
+//			InputStream is = multipartFile.getInputStream();
+//			String contentType = multipartFile.getContentType();
+//			return uploadFile(is, fileName, contentType);
+//		} catch (IOException e) {
+//			throw new FileException("Erro de IO: " + e.getMessage());
+//		}
+//	}
+
+//	public URI uploadFile(InputStream is, String fileName, String contentType) {
+//		try {
+//			ObjectMetadata meta = new ObjectMetadata();
+//			meta.setContentType(contentType);
+//			LOG.info("Iniciando upload");
+//			s3client.putObject(bucketName, fileName, is, meta);
+//			LOG.info("Upload finalizado");
+//			return s3client.getUrl(bucketName, fileName).toURI();
+//		} catch (URISyntaxException e) {
+//			throw new FileException("Erro ao converter URL para URI");
+//		}
+//	}
+}
